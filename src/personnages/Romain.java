5bc80b58d3d10a846d4b67106974150f4f4ea7ca
Nolawi Gebrekirstos <gbn5079a@ensfsi.onmicrosoft.com>
2024-10-01 17:25:23 +0200

    invariant pre post conditions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -5,10 +5,15 @@ public class Romain {
 	private int force;
 
 	public Romain(String nom, int force) {
+		assert invariantForce(force);
 		this.nom = nom;
 		this.force = force;
+		
 	}
-
+	private boolean invariantForce(int force) {
+		return force>0;
+	}
+	
 	public String getnom() {
 		return nom;
 
@@ -23,11 +28,18 @@ public class Romain {
 	}
 
 	public void recevoirCoup(int forceCoup) {
+		assert invariantForce(force);
 		force -= forceCoup;
 		if (force > 0) {
 			parler("Aie");
 		} else {
 			parler("J'abandonne...");
 		}
+	}
+	public static void main(String[] args) {
+		Romain minus=new Romain("Minus", 6);
+		minus.parler("UN GAU... UN GAUGAU...");
+
+
 	}
 }
